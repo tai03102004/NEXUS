32e88674474f83c413649644b89301569e6071c4
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2024-09-17 02:00:10 +0700

    update

--- NEXUS/client/src/components/Transactions.tsx
+++ NEXUS/client/src/components/Transactions.tsx
@@ -1,11 +1,6 @@
-import React, {useContext} from "react";
-
-import { TransactionContext } from "../context/TransactionContext";
-
 import dummyData from "../utils/dummyData.ts";
 
 import { TransactionCard } from "../Interface/TransactionCard";
-import { shortenAddress } from "../utils/shortenAddress.ts";
 
 import useFetch from "../hooks/useFetch.tsx";
 
@@ -67,21 +62,18 @@ const TransactionCard = (props: TransactionCard) => {
 
 function Transactions() {
 
-    const currentAmount = useContext(TransactionContext);
-
     return (
         <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
             <div className = "flex flex-col md:p-12 py-12 px-4">
                 { /* Lastest Transactions */ 
-                    currentAmount ? (
-                        <h3 className = "text-white text-3xl text-center my-2"  data-aos="fade-zoom-in" data-aos-duration = "1200">
-                            Lastest Transactions
-                        </h3>
-                    ) : (
-                        <h3 className = "text-white text-3xl text-center my-2">
-                            Context your amount to see the lastest transactions
-                        </h3>
-                    )
+                    <h3 className = "text-white text-3xl text-center my-2"  data-aos="fade-zoom-in" data-aos-duration = "1200">
+                        Lastest Transactions
+                    </h3>
+                    // ) : (
+                    //     <h3 className = "text-white text-3xl text-center my-2">
+                    //         Context your amount to see the lastest transactions
+                    //     </h3>
+                    // )
                 }
                 <div className="flex flex-wrap justify-center items-center mt-10">
                     {dummyData.reverse().map((transactions,i) => (
